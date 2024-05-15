b19513aa1dc5a8f2a4f73c5d89fd59a825b3e78f
Saran Kumar S <sarankumar.95977@gmail.com>
2024-05-15 09:37:01 +0530

    No task displayed update

--- src/Components/TaskComponents/AllTasks.jsx
+++ src/Components/TaskComponents/AllTasks.jsx
@@ -14,9 +14,24 @@ const AllTasks = () => {
 
   return (
     <div className='alltasks_container'>
-      {allTaskDetails.map((taskDtls) => (
-        <Task taskDetails={taskDtls} handleTaskOperations={handleTaskOperations}/>
-      ))}
+      {allTaskDetails.length?
+        allTaskDetails.map((taskDtls) => (
+          <Task taskDetails={taskDtls} handleTaskOperations={handleTaskOperations}/>
+        )):
+        <p className='notask_displayed_text'>
+          {
+            (StatesAndFunctions.activeNavlink==="All")?
+            "No tasks assigned currently":
+            (StatesAndFunctions.activeNavlink==="In progress")?
+            "No tasks in progress":
+            (StatesAndFunctions.activeNavlink==="Completed")?
+            "No completed tasks yet":
+            (StatesAndFunctions.activeNavlink==="Overdue")?
+            "You're all caught up! No tasks are overdue":
+            ""
+          }
+        </p>
+      }
     </div>
   )
 };
